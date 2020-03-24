16255a0366df0f2f78d728611341e406711e40c6
Tj-Palm <48101800+Tj-Palm@users.noreply.github.com>
2020-03-24 11:34:17 +0100

    1 userstory

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,34 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError
+}
+from "../../node_modules/axios/index"
+import Axios from "../../node_modules/axios/index";
+
+interface IMusicRecords {
+    Title: String;
+    Artist: String;
+    Duration: Number;
+    yearOfPublication: number;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let baseuri: string = "https://drmusicrecords.azurewebsites.net/api/Musicrecord";
+
+let showAll: HTMLButtonElement = <HTMLButtonElement> document.getElementById("ShowElements")
+showAll.addEventListener("click", ShowAllRecords)
+
+function ShowAllRecords(){
+Axios.get(baseuri)
+.then(function (response: AxiosResponse): void {
+    console.log("Getting records v1.0");
+    let data: IMusicRecords[] = response.data;
+    console.log(data);
+    element.innerHTML = JSON.stringify(data);
+})
+.catch(function (error: AxiosError): void {
+    contentelement.innerHTML = error.message;
+})
+};
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let contentelement: HTMLDivElement = <HTMLDivElement> document.getElementById("contentelement")
